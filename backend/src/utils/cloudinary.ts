01fab9209a7d342a6b900e76442f4c93df2b258a
Dey11 <shreyandey11@gmail.com>
2024-04-13 14:57:07 +0530

    add better folder names

--- backend/src/utils/cloudinary.ts
+++ backend/src/utils/cloudinary.ts
@@ -10,13 +10,15 @@ cloudinary.config({
 });
 
 const uploadToCloudinary = async (imageName: string, username: string) => {
+  const fileType = imageName.split(".")[1];
+  const fileName = imageName.split(".")[0];
   cloudinary.uploader.upload(
     `./public/images/${imageName}`,
     {
-      public_id: imageName,
+      public_id: fileName,
       use_asset_folder_as_public_id_prefix: true,
-      asset_folder: username,
-      use_filename_as_display_name: true,
+      asset_folder: `${username}/${fileType}`,
+      // use_filename_as_display_name: true,
     },
     function (error, result) {
       if (error) {
